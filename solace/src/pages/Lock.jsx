8fcd09b27e259b9e6a7f061d9e18e54c6ae423dc
Sean Laurent <cajuluokeke@gmail.com>
2023-04-07 18:41:21 -0700

    Lock Updates

--- solace/src/pages/Lock.jsx
+++ solace/src/pages/Lock.jsx
@@ -3,11 +3,11 @@ import logo_spinning from '../assets/logo_spinning.mp4'
 
 const Lock = () => {
     return (
-        <div className="bg-[#2323F2] text-[#F3F3F3] flex flex-col justify-center items-center h-[100vh] min-h-[600px] max-h-[1000px] gap-y-5 overflow-hidden">
-            <video autoPlay loop muted className="object-cover w-[60vh] h-[60vh] max-w-[800px] max-h-[800px] min-w-[400px] min-h-[400px] overflow-hidden">
+        <div className="bg-[#2323F2] text-[#F3F3F3] flex flex-col items-center h-[100vh] min-h-[600px] gap-y-5 pt-40 overflow-hidden">
+            <video autoPlay loop muted className="object-cover w-[40vh] h-[40vh] max-w-[400px] max-h-[400px] min-w-[200px] min-h-[200px] overflow-hidden">
                 <source src={logo_spinning} type="video/mp4" />
             </video>
-            <h1 className="font-roboto text-[2rem] sm:text-[4rem]">Coming Soon!</h1>
+            <h1 className="font-roboto">Coming Soon!</h1>
         </div>
     )
 }
